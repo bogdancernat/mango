2bf9238ad1cd5f39af5fccd71c23e638379cd9a2
Bogdan Cernat <cernat.bogdan.stefan@gmail.com>
2014-11-22 18:50:24 +0200

    add character movement with vertical map generation

---
+++ Assets/b_character_movement.js
@@ -0,0 +1,66 @@
+﻿#pragma strict
+
+var characterSpeed: float;
+private var background: GameObject;
+private var cameraHeight: float;
+
+private var planes: ArrayList;
+private var backgroundRotation;
+private var backgroundHeight: float;
+
+function Start () {
+	characterSpeed = 2f;
+	backgroundRotation = Quaternion.AngleAxis(270, Vector3.right);
+	cameraHeight = Camera.main.orthographicSize * 2f;
+  	
+  	background = GameObject.Find("Background");
+	planes = new ArrayList();
+	planes.Add(background.gameObject);
+	
+  	var renderer: Renderer = background.gameObject.GetComponent(Renderer);
+	backgroundHeight = renderer.bounds.size.y;
+	
+//  	var mesh: Mesh = background.GetComponent(MeshFilter).mesh;
+//  	var vertices : Vector3[] = mesh.vertices;
+//	var uvs : Vector2[]  = new Vector2[vertices.Length];
+//	var bounds : Bounds = mesh.bounds;
+//	
+//	for (var i = 0; i < uvs.Length; i++)
+//		uvs[i] = Vector2 (vertices[i].x / bounds.size.x
+//						 ,vertices[i].z / bounds.size.x);
+//	mesh.uv = uvs;
+//	
+	
+}
+
+function Update () {
+	transform.Translate(transform.up * characterSpeed * Time.deltaTime);
+//	Debug.Log(transform.position.y);
+	var backgroundRef: GameObject = planes[planes.Count - 1];
+	
+	if((backgroundRef.transform.position.y + backgroundHeight) < (transform.position.y + cameraHeight * 1.5)) {
+		var clone: GameObject = GameObject.Instantiate(background, backgroundRef.transform.position + backgroundHeight * Vector3.up, backgroundRotation);
+		planes.Add(clone.gameObject);
+		clone.name = "Background-"+planes.Count;
+	}
+	if(planes.Count > 2){
+		var toDestroy: GameObject = planes[0];
+		planes = planes.GetRange(1, 2);
+		GameObject.Destroy(toDestroy);
+		background = planes[0];
+		background.name = "Background-2";
+	}
+//	Debug.Log(backgroundRef.transform.position);
+	
+	
+//	Debug.Log(backgroundRefrence.transform.position);
+}
+
+//  var mesh : Mesh = GetComponent(MeshFilter).mesh;
+//	var vertices : Vector3[] = mesh.vertices;
+//	var uvs : Vector2[]  = new Vector2[vertices.Length];
+//	var bounds : Bounds = mesh.bounds;
+//	for (var i = 0; i < uvs.Length; i++)
+//		uvs[i] = Vector2 (vertices[i].x / bounds.size.x
+//						 ,vertices[i].z / bounds.size.x);
+//	mesh.uv = uvs;
---
+++ Assets/b_plane_init.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+function Start () {
+
+//	var height = Camera.main.orthographicSize * 2.0;
+//	var width = height * Screen.width / Screen.height;
+//	transform.localScale = Vector3(width, height, 0.1);
+//	Debug.Log(height);
+//	Debug.Log(width);
+
+//	Camera cam = Camera.main;
+//
+//	float pos = cam.nearClipPlane + 10.0f;
+//
+//	transform.position = cam.transform.position + cam.transform.forward * pos;
+//	transform.LookAt (cam.transform);
+//	transform.Rotate (90.0f, 0.0f, 0.0f);
+//
+//	float h = (Mathf.Tan(cam.fov * Mathf.Deg2Rad * 0.5f) * pos * 2f) / 10.0f;
+//
+//	transform.localScale = new Vector3(h*cam.aspect,1.0f, h);
+	
+}
+
+function Update () {
+
+}
