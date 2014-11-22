f7ff70db10d49d3050a3c2baf1a2977d40a0a902
Bogdan Cernat <cernat.bogdan.stefan@gmail.com>
2014-11-22 20:47:54 +0200

    combine mara's movement speed with my scene

--- Assets/b_gridBindings.js
+++ Assets/b_gridBindings.js
@@ -1,26 +1,27 @@
 ﻿#pragma strict
-//var layers = new Array() [
-//	['1','2','3','4','5','6','7','8','9','0'],
-//	['q','w','e','r','t','y','u','i','o','p'],
-//	['a','s','d','f','g','h','j','k','l',';'],
-//	['z','x','c','v','b','n','m',',','.','/']
-//];
-var targetKeys = ['1','2','3','4','5','6','7','8','9','0','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l',';','z','x','c','v','b','n','m',',','.','/'];
+var layers: Array;
+var targetKeys: Array;
+var player: GameObject;
 
 function Start () {
-
+////	player = GameObject.Find('');
+//	layers = new Array() [
+//		['1','2','3','4','5','6','7','8','9','0'],
+//		['q','w','e','r','t','y','u','i','o','p'],
+//		['a','s','d','f','g','h','j','k','l',';'],
+//		['z','x','c','v','b','n','m',',','.','/']
+//	];
+//	targetKeys = ['1','2','3','4','5','6','7','8','9','0','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l',';','z','x','c','v','b','n','m',',','.','/'];
 }
 
 function Update () {
-
+	
 }
 
 function OnGUI() {
-	var e : Event = Event.current;
-	if (e.isKey && System.Array.IndexOf(targetKeys, System.Convert.ToString(e.character)) >= 0) {
-		Debug.Log("Detected key code: " + e.character);
-		Debug.Log(Screen.height);
-	}
-	
+//	var e : Event = Event.current;
+//	if (e.isKey && System.Array.IndexOf(targetKeys, System.Convert.ToString(e.character)) >= 0) {
+//		Debug.Log("Detected key code: " + e.character);
+//	}
 	
 }
